ec6b316c3a75aac4be9ca19c4ccb268b193a45b1
marielmay1 <mariel.alexandra.may@gmail.com>
2021-09-26 22:23:31 -0700

    feature: s3 deploy in ci

--- src/MARIEL_MAYBANKINGAPP/home.js
+++ src/MARIEL_MAYBANKINGAPP/home.js
@@ -7,7 +7,7 @@ function Home() {
             title="Welcome to Mariel's Bank!"
             text=""
             body={(<img src="bank.png" className="img-fluid"
-                alt="Responsive image"/>)}
+                        alt="Responsive image"/>)}
       />
     );
 }
